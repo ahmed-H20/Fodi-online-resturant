86fc7d001c512e73422af9fd4961a5181fca89a5
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-08-06 03:24:32 +0300

    Install packges

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose');
 
 //Middleware
 app.use(cors());
-app.use(express().json());
+app.use(express.json());
 
-mongoose.connect(`mongodb+srv://ahmedheshamahah8:xS9WGApnCwM7tzjw@resturant-fodi.q0nh8vw.mongodb.net/?retryWrites=true&w=majority&appName=Resturant-fodi`)
+mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@resturant-fodi.q0nh8vw.mongodb.net/?retryWrites=true&w=majority&appName=Resturant-fodi`)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
