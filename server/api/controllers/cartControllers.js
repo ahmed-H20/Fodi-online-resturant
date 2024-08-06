defbd39e48a2b0d6a80fc5b3f624fb4dfbf54c86
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-08-06 10:22:26 +0300

    Some changes

--- server/api/controllers/cartControllers.js
+++ server/api/controllers/cartControllers.js
@@ -6,6 +6,7 @@ const getCartByEmail = async(req, res) => {
         const email = req.query.email;
         const query = {email: email};
         const result = await Carts.find(query).exec();
+        res.status(200).json(result);
     }
     catch (error){
         res.status(500).json({message : error.message});
