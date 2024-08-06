c69980157570ec863fba01a81bf9bc76c10e4af3
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-08-06 07:41:58 +0300

    Make some routes

--- server/api/routes/cartRouter.js
+++ server/api/routes/cartRouter.js
@@ -6,6 +6,10 @@ const router = express.Router();
 const cartController = require('../controllers/cartControllers');
 
 // Routers
-router.get('/', cartController.getCartByEmail)
+router.get('/', cartController.getCartByEmail);
+router.post('/', cartController.addToCart);
+router.delete('/:id', cartController.deleteCart);
+router.put('/:id', cartController.updateCart);
+router.get('/:id', cartController.getSingleCart);
 
 module.exports = router;
