2a69f683f9d2157ea6ef3443c48721985d883773
ahmed-H20 <ahmedheshamahah8@gmail.com>
2024-08-06 03:19:10 +0300

    Some installs

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config()
+const port = process.env.PORT || 3000
+const mongoose = require('mongoose');
+
+//Middleware
+app.use(cors());
+app.use(express().json());
+
+mongoose.connect(`mongodb+srv://ahmedheshamahah8:xS9WGApnCwM7tzjw@resturant-fodi.q0nh8vw.mongodb.net/?retryWrites=true&w=majority&appName=Resturant-fodi`)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
